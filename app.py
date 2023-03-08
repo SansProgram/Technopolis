f17be5b731843857b50ef036808a146c72b8d6d5
Sanele Khanyile <98615661+SansProgram@users.noreply.github.com>
2023-03-08 21:41:00 +0000

    Added html code + CSS code + image for home page dynamic data. Basic layout of how it will look (colours not finalised).

--- app.py
+++ app.py
@@ -1,10 +1,46 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
+TUTOR =[
+  {
+    'id': 1,
+    'title': 'Tutor Position',
+    'location': 'DUT Steve Biko, ML Sutan, Ritson Campuses',
+    'salary': 'R1500.00',
+    'department': ''
+  },
+  {
+    'id': 2,
+    'title': 'Teacher Assistant',
+    'location': 'DUT Steve Biko, ML Sutan, Ritson Campuses',
+    'salary': 'R1000.00',
+    'department': ''
+  },
+  {
+    'id': 1,
+    'title': 'Tutor Position',
+    'location': 'DUT Steve Biko, ML Sutan, Ritson Campuses',
+    'salary': 'R1500.00',
+    'department': ''
+  },
+  {
+    'id': 1,
+    'title': 'Tutor Position',
+    'location': 'DUT Steve Biko, ML Sutan, Ritson Campuses',
+    'salary': 'R00.00',
+    'department': ''
+  }
+]
+
 @app.route("/")
 def hello_world():
-  return render_template('home.html')
+  return render_template('home.html', 
+                         tutor=TUTOR,
+                        company_name='Technopolis')
+@app.route("/api/tutor")
+def list_tutor():
+  return jsonify(TUTOR)
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
